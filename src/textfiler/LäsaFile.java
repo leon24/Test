42573bd6c9a822d142fe13a91e027477f7984f05
LeonsBärbara <LeonsBrbara@LAPTOP-B71PIEPN>
2016-09-05 13:02:46 +0200

    First Andra commit

--- src/textfiler/LäsaFile.java
+++ src/textfiler/LäsaFile.java
@@ -12,14 +12,19 @@ public class LäsaFile {
         System.out.println("Vilka i vilket intervall vill du läsa in filer? ");
         String fileNamn = scan.nextLine();
         File dir = new File("D:\\Users\\LeonsBärbara\\Documents\\NetBeansProjects\\Textfiler\\läsafiler");
-        byte [] buffer = new byte[1000];
-        FileInputStream inputStream = new FileInputStream(dir);
-        int total = 0;
-        int nRead = 0;
-        while((nRead = inputStream.read(buffer)) != -1){
-            System.out.println(new String(buffer));
+        for (File file : dir.listFiles()) {
+            Scanner s = new Scanner(file);
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNext()){
+            list.add(s.next());
+}
+        System.out.println(list);
+        s.close();
+        
+         
         }
-        inputStream.close();
-        System.out.println("Read " + total + " bytes"); 
     }
 }
+
+
+
