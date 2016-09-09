0f045a6d0804c029d07a993af063e11ac4d75717
LeonsBärbara <LeonsBrbara@LAPTOP-B71PIEPN>
2016-09-09 08:56:35 +0200

    Add comment

--- src/textfiler/LäsaFile.java
+++ src/textfiler/LäsaFile.java
@@ -9,22 +9,44 @@ public class LäsaFile {
     }
     private static void readFile() throws FileNotFoundException, IOException{
         Scanner scan = new Scanner(System.in);
+        int startswith;
+         int endwith;
         System.out.println("Vilka i vilket intervall vill du läsa in filer? ");
-        String fileNamn = scan.nextLine();
-        File dir = new File("D:\\Users\\LeonsBärbara\\Documents\\NetBeansProjects\\Textfiler\\läsafiler");
-        for (File file : dir.listFiles()) {
-            Scanner s = new Scanner(file);
-        ArrayList<String> list = new ArrayList<String>();
-        while (s.hasNext()){
-            list.add(s.next());
-}//comment
-        System.out.println(list);
-        s.close();
-        
+        startswith = scan.nextInt();
+        endwith = scan.nextInt();
+        File folder = new File("D:\\Users\\LeonsBärbara\\Documents\\NetBeansProjects\\Textfiler\\läsafiler");
+        File[] listOfFiles = folder.listFiles();
+         for (int i = 0; i < listOfFiles.length; i++)
+        System.out.println(listOfFiles[i].getName());
          
-        }
+         
+         
+        
+//       File dir = new File(TargetD);
+//       for (File file : ) {
+//            Scanner s = new Scanner(file);
+//        
+//        ArrayList<String> list = new ArrayList<String>();
+//        while (s.hasNext()){
+//            list.add(s.next());
+//        }
+//        System.out.println(list);
+//        }
     }
-}
+    }
+//        for (File file : dir.listFiles()) {
+//            Scanner s = new Scanner(file);
+//        ArrayList<String> list = new ArrayList<String>();
+//        while (s.hasNext()){
+//            list.add(s.next());
+//}
+//        System.out.println(list);
+//        s.close();
+//        
+//         
+//        }
+    
+
 
 
 
